e0302a2e6d2d1ffdbe4c5f0890047c0fd7923699
peppimakinen <111729175+peppimakinen@users.noreply.github.com>
2024-04-17 21:35:49 +0300

    patient home continued

--- Frontend/patienthome.js
+++ Frontend/patienthome.js
@@ -1,62 +1,13 @@
+import { renderCalendar } from "./rendercalendar";
 import { fetchData } from "./fetch.js";
 
-const daysTag = document.querySelector(".days"),
-  currentDate = document.querySelector(".currentDate"),
-  prevNextIcon = document.querySelectorAll(".calendarHeader span");
+// Render calendar when page is loaded
+window.addEventListener("load", () => {
+  renderCalendar();
+});
 
-// getting new date, current year and month
-let date = new Date(),
-  currYear = date.getFullYear(),
-  currMonth = date.getMonth();
-
-// storing full name of all months in array
-const months = [
-  "Tammikuu",
-  "Helmikuu",
-  "Maaliskuu",
-  "Huhtikuu",
-  "Toukokuu",
-  "Kesäkuu",
-  "Heinäkuu",
-  "Elokuu",
-  "Syyskuu",
-  "Lokakuu",
-  "Marraskuu",
-  "Joulukuu",
-];
-
-const renderCalendar = () => {
-  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
-    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
-  let liTag = "";
-
-  for (let i = firstDayofMonth; i > 0; i--) {
-    // creating li of previous month last days
-    liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
-  }
-
-  for (let i = 1; i <= lastDateofMonth; i++) {
-    // creating li of all days of current month
-    // adding active class to li if the current day, month, and year matched
-    let isToday =
-      i === date.getDate() &&
-      currMonth === new Date().getMonth() &&
-      currYear === new Date().getFullYear()
-        ? "active"
-        : "";
-    liTag += `<li class="${isToday}">${i}</li>`;
-  }
-
-  for (let i = lastDayofMonth; i < 6; i++) {
-    // creating li of next month first days
-    liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
-  }
-  currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
-  daysTag.innerHTML = liTag;
-};
-renderCalendar();
+// Render calendar when previous or next buttons are clicked
+const prevNextIcon = document.querySelectorAll(".calendarHeader span");
 
 prevNextIcon.forEach((icon) => {
   // getting prev and next calendarHeader
