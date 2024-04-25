bcf3aab773d678f2333b4a06f07009c6216cd514
peppimakinen <111729175+peppimakinen@users.noreply.github.com>
2024-04-25 16:10:10 +0300

    fixed date conversion

--- Frontend/home/convertday.js
+++ Frontend/home/convertday.js
@@ -1,7 +1,7 @@
 // Function to convert selected date to yyyy-mm-dd format
-function convertToYYYYMMDD(selectedDate) {
+function convertToYYYYMMDD(date) {
   // Split the date into day, month, and year
-  const [day, month, year] = selectedDate.split(".");
+  const [day, month, year] = date.split(".");
   // Construct the date string in yyyy-mm-dd format
   const formattedDate = `${year}-${month.padStart(2, "0")}-${day.padStart(
     2,
@@ -10,13 +10,4 @@ function convertToYYYYMMDD(selectedDate) {
   return formattedDate;
 }
 
-// Function to convert selected date to dd.mm.yyyy format
-function convertToDDMMYYYY(date) {
-  // Split the date into year, month, and day
-  const [year, month, day] = date.split(".");
-  // Construct the date string in dd.mm.yyyy format
-  const formattedDate = `${day.padStart(2, "0")}.${month.padStart(2, "0")}.${year}`;
-  return formattedDate;
-}
-
-export { convertToYYYYMMDD, convertToDDMMYYYY };
+export { convertToYYYYMMDD };
