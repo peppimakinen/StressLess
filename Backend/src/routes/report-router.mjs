d25e3c3245c5fb25156d7ea3a20110458819d858
Alekkivi <Aleksi.Kivilehto@metropolia.fi>
2024-04-28 20:53:49 +0300

    Added test for auth/patient-login and added apidoc for report-router

--- Backend/src/routes/report-router.mjs
+++ Backend/src/routes/report-router.mjs
@@ -1,25 +1,180 @@
-import {
-  validationErrorHandler,
-  onlyForPatientWhoCompletedSurvey,
-  verifyRightToViewPatientsData,
-  onlyForDoctorHandler,
-} from '../middlewares/error-handler.mjs';
+/* eslint-disable max-len */
+
 import {authenticateToken} from '../middlewares/authentication.mjs';
 import {param} from 'express-validator';
+import express from 'express';
 import {
-  getAvailableWeeks,
-  getSpecificReport,
   getAvailablePatientReports,
   getPatientsReport,
+  getAvailableWeeks,
+  getSpecificReport,
 } from '../controllers/report-controller.mjs';
-import express from 'express';
+import {
+  onlyForPatientWhoCompletedSurvey,
+  verifyRightToViewPatientsData,
+  validationErrorHandler,
+  onlyForDoctorHandler,
+} from '../middlewares/error-handler.mjs';
 
-// eslint-disable-next-line new-cap
 const reportRouter = express.Router();
+/**
+ * @apiDefine NoEntryDataForReportsError
+ * @apiError NoEntryDataForReportsError No user data, can't calculate report.
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": {
+ *         "message": "No entries found with user_id=1",
+ *         "status": 404
+ *       }
+ *     }
+ */
+/**
+ * @apiDefine DoctorDoesntFindReportError
+ * @apiError DoctorDoesntFindReportError Patient has no report with provided report ID
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": {
+ *         "message": "There is no report_id=1 for patient_id=1",
+ *         "status": 404
+ *       }
+ *     }
+ */
+/**
+ * @apiDefine NoReportsAvailableError
+ * @apiError NoReportsAvailableError Patient has no reports to find
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": {
+ *         "message": "No reports found for patient_id=1",
+ *         "status": 404
+ *       }
+ *     }
+ */
+/**
+ * @apiDefine OnlyForDoctorsError
+ * @apiError OnlyForDoctorsError Non-doctor user attempting to access restricted endpoint.
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 403 Forbidden
+ *     {
+ *       "error": {
+ *         "message": "This endpoint is only for StressLess doctor users",
+ *         "status": 403
+ *       }
+ *     }
+ */
+
+/**
+ * @apiDefine ForbiddenDoctorRequest
+ * @apiError ForbiddenDoctorRequest Doctor user attempting to access patient data without permission.
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 403 Forbidden
+ *     {
+ *       "error": {
+ *         "message": "Requested patient is not sharing data with doctor_id=5",
+ *         "status": 403
+ *       }
+ *     }
+ */
+/**
+ * @apiDefine ReportNotFoundError
+ * @apiError ReportNotFoundError Report could not be found with valid report ID
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": {
+ *         "message": "Report_id=6 not found",
+ *         "status": 404
+ *       }
+ *     }
+ */
+/**
+ * @apiDefine InvalidReportIdError
+ * @apiError InvalidReportIdError Invalid syntax for report ID in the URL
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": {
+ *         "message": "Bad Request",
+ *         "status": 400
+ *         "errors": [
+ *             {
+ *                  "field":"report_id",
+ *                  "message":"invalid value"
+ *             }
+ *         ]
+ *       }
+ *     }
+ */
+/**
+ * @api {get} api/reports/available-weeks Get past week reports
+ * @apiVersion 1.0.0
+ * @apiName getAvailableWeeks
+ * @apiGroup Reports
+ * @apiPermission onlyPatients
+ *
+ * @apiDescription Fetch weeks that contain reports
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+ *          [
+ *             {
+ *                "user_id": 6,
+ *                "report_id": 1,
+ *                "week_number": 6,
+ *                "week_start_date": "2024-02-05",
+ *                "week_end_date": "2024-02-11"
+ *              }
+ *          ]
+ *
+ * @apiUse NoEntryDataForReportsError
+ * @apiUse SurveyNotCompletedError
+ * @apiUse InvalidTokenError
+ * @apiUse TokenMissingError
+ */
 reportRouter
   .route('/available-weeks')
   .get(authenticateToken, onlyForPatientWhoCompletedSurvey, getAvailableWeeks);
-
+/**
+ * @api {get} api/reports/report_id Get report
+ * @apiVersion 1.0.0
+ * @apiName getSpecificReport
+ * @apiGroup Reports
+ * @apiPermission onlyPatients
+ *
+ * @apiDescription Get a specific weekly report using report ID
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+ *          {
+ *             "report_id": 1,
+ *             "week_number": 6,
+ *             "week_start_date": "2024-02-05",
+ *             "week_end_date": "2024-02-11",
+ *             "red_percentage": "14.29",
+ *             "green_percentage": "14.29",
+ *             "yellow_percentage": "14.29",
+ *             "gray_percentage": "57.14",
+ *             "monday_si": "0.00",
+ *             "tuesday_si": "0.00",
+ *             "wednesday_si": "0.00",
+ *             "thursday_si": "0.00",
+ *             "friday_si": "8.92",
+ *             "saturday_si": "11.69",
+ *             "sunday_si": "9.36",
+ *             "week_si_avg": "4.28",
+ *             "previous_week_si_avg": null,
+ *             "created_at": "2024-04-28"
+ *           }
+ *
+ * @apiUse InvalidReportIdError
+ * @apiUse ReportNotFoundError
+ * @apiUse SurveyNotCompletedError
+ * @apiUse InvalidTokenError
+ * @apiUse TokenMissingError
+ */
 reportRouter
   .route('/:report_id')
   .get(
@@ -29,7 +184,34 @@ reportRouter
     validationErrorHandler,
     getSpecificReport,
   );
-
+/**
+ * @api {get} api/reports/doctor/available-weeks/patient_id Get patients available week reports
+ * @apiVersion 1.0.0
+ * @apiName getAvailablePatientReports
+ * @apiGroup Reports
+ * @apiPermission onlyDoctors
+ *
+ * @apiDescription Retrieve past reports from consenting patients
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+*          [
+ *             {
+ *                "user_id": 6,
+ *                "report_id": 1,
+ *                "week_number": 6,
+ *                "week_start_date": "2024-02-05",
+ *                "week_end_date": "2024-02-11"
+ *              }
+ *          ]
+ *
+ * @apiUse NoReportsAvailableError
+ * @apiUse ForbiddenDoctorRequest
+ * @apiUse OnlyForDoctorsError
+ * @apiUse InvalidReportIdError
+ * @apiUse InvalidTokenError
+ * @apiUse TokenMissingError
+ */
 reportRouter
   .route('/doctor/available-weeks/:patient_id')
   .get(
@@ -40,7 +222,45 @@ reportRouter
     verifyRightToViewPatientsData,
     getAvailablePatientReports,
   );
-
+/**
+ * @api {get} api/reports/doctor/specific-report/report_id/patient_id Get patients specific week report
+ * @apiVersion 1.0.0
+ * @apiName getPatientsReport
+ * @apiGroup Reports
+ * @apiPermission onlyDoctors
+ *
+ * @apiDescription Get patients specific week report using patient and report ID's
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+ *          {
+ *             "report_id": 1,
+ *             "week_number": 6,
+ *             "week_start_date": "2024-02-05",
+ *             "week_end_date": "2024-02-11",
+ *             "red_percentage": "14.29",
+ *             "green_percentage": "14.29",
+ *             "yellow_percentage": "14.29",
+ *             "gray_percentage": "57.14",
+ *             "monday_si": "0.00",
+ *             "tuesday_si": "0.00",
+ *             "wednesday_si": "0.00",
+ *             "thursday_si": "0.00",
+ *             "friday_si": "8.92",
+ *             "saturday_si": "11.69",
+ *             "sunday_si": "9.36",
+ *             "week_si_avg": "4.28",
+ *             "previous_week_si_avg": null,
+ *             "created_at": "2024-04-28"
+ *           }
+ *
+ * @apiUse DoctorDoesntFindReportError
+ * @apiUse ForbiddenDoctorRequest
+ * @apiUse OnlyForDoctorsError
+ * @apiUse InvalidReportIdError
+ * @apiUse InvalidTokenError
+ * @apiUse TokenMissingError
+ */
 reportRouter
   .route('/doctor/specific-report/:report_id/:patient_id')
   .get(
--- Backend/tests/patient-auth/endpoints/stressless-endpoints.robot
+++ Backend/tests/patient-auth/endpoints/stressless-endpoints.robot
@@ -8,9 +8,6 @@ Library    Collections
 *** Variables ***
 ${Username}    crypt:PcD1tAnbMlsIge3//bWX4nPhyyB0Lu0DPXPDipMgv3tIcWPcfvgM8jV4XFsfW2vEqheWyVv+A2cg0ChpDESQKDWy5Y0k0W3LEgMriR0p
 ${Password}    crypt:tSNi8tTS6pD1IMEy9EfvtwdUdwF3xqzMd/EZMEfhtTw04yQGm3XXaODegPSQ7uIFyRRK8wJj3NrGbvzjp8OZAw==
-${token}    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFsZWtzaS5raXZpbGVodG9AbWV0cm9wb2xpYS5maSIsImZ1bGxfbmFtZSI6IkFsZWtzaSBLaXZpbGVodG8iLCJ1c2VyX2xldmVsIjoicGF0aWVudCIsImNyZWF0ZWRfYXQiOiIyMDI0LTA0LTI4VDExOjM4OjMwLjAwMFoiLCJzdXJ2ZXlDb21wbGV0ZWQiOnRydWUsInRva2VuIjoiZXlKcmFXUWlPaUpRVkVSUlVHWkxialUyYlhCNVJVOXBjSFZ4T0dSRlVEQlBlWGxKTVdoMVdsUXdNVkpWZWtkWFVTdFJQU0lzSW1Gc1p5STZJbEpUTWpVMkluMC5leUpoZEY5b1lYTm9Jam9pVGtwSWRuRlZhMFkzVWsxaVpVeHNWRTlsT1dkT1FTSXNJbk4xWWlJNkltVmtNamc0TW1VNUxXUXdZVEF0TkRnek1pMDRNMlF4TFRjME5tRXlPVEl6TVRka05DSXNJbVZ0WVdsc1gzWmxjbWxtYVdWa0lqcDBjblZsTENKcGMzTWlPaUpvZEhSd2N6cGNMMXd2WTI5bmJtbDBieTFwWkhBdVpYVXRkMlZ6ZEMweExtRnRZWHB2Ym1GM2N5NWpiMjFjTDJWMUxYZGxjM1F0TVY5MVVXMUJaelExZUhraUxDSmpiMmR1YVhSdk9uVnpaWEp1WVcxbElqb2laV1F5T0RneVpUa3RaREJoTUMwME9ETXlMVGd6WkRFdE56UTJZVEk1TWpNeE4yUTBJaXdpWjJsMlpXNWZibUZ0WlNJNklrRnNaV3R6YVNJc0ltRjFaQ0k2SWpjME5UY3hjR1JvZFdNM2RuWmhhelIwYkRRMWRYUnpPSFU0SWl3aVpYWmxiblJmYVdRaU9pSTBOR014TWpSa05DMWlObUZpTFRRM05qZ3RPR0UwTlMweU9URTJNRFkwWTJWbE1qa2lMQ0owYjJ0bGJsOTFjMlVpT2lKcFpDSXNJbUYxZEdoZmRHbHRaU0k2TVRjeE5ETXdPREV4Tml3aVpYaHdJam94TnpFME16RXhOekUyTENKcFlYUWlPakUzTVRRek1EZ3hNVFlzSW1aaGJXbHNlVjl1WVcxbElqb2lTMmwyYVd4bGFIUnZJaXdpWlcxaGFXd2lPaUpoYkdWcmMya3VhMmwyYVd4bGFIUnZRRzFsZEhKdmNHOXNhV0V1Wm1raWZRLk1QVXl5c3ZsZXhXT1gzMDBIeE8yWWlGWDBUdXlObVplSTFWU0Y2S0hfdWpWcjFfbXN4cmQzbGFHTzZwUmI5NUNFZ0twOUd6WmNFb0p6SDBYYkhRR0hkNlpaOG1aTjhBVzBDMHVTSU5Dd0NaakNkS1AtRzB6RmsyVmxNdkNNWWV1QzBkV1k5a0p0Y3RhajRiZFNRX3I3ZEVHT2tjaTlRcjVRd3Z4dmFvMEFtclNZam9BTjZFTDdOcDVGV2Z1SElSUzctYXZ5dDFjaEJyYllNRGtXcmhKQXROWUttLUp0WnU0d25CZENIWUwxdHNmN0NSZmdSUE1WNVBaMWY3bUpTVEJsdFhjUDBtd29pVWdjdFZDZ1pYMmVUVlBWOFhoQm1iTTU0bmotaFZNYldnRkF0QlBoTTJPTkJfZXloaFFsVlVzRnFzY1otZUp3SjJ3TUYyVy1QY3ptQSIsImlhdCI6MTcxNDMwODExNywiZXhwIjoxNzE0MzExNzE3fQ.QSJiBoTHhaa0q5H-AUIA3WX6a9a1XWaxGzsjjzOMyGE
-${surveyStatus}    True
-&{headers}    Content-Type=application/json   Authorization=Bearer ${token}
 ${question1}    On a scale from 1 to 5, how stressed are you?
 ${question2}    Does stress affect your sleep?
 ${answer1}  Around a 3
@@ -60,6 +57,19 @@ Get available week reports
 
 
 *** Test Cases ***
+Authenticate as Patient
+    ${body}    Create Dictionary    username=${Username}   password=${Password}
+    ${response}    POST    url=http://127.0.0.1:3000/api/auth/patient-login    json=${body}
+    Log    ${response.json()}
+    ${token}    Set Variable    ${response.json()}[token]
+    ${surveyStatus}    Set Variable    ${response.json()}[user][surveyCompleted]
+    &{headers}    Create Dictionary    Content-Type=application/json   Authorization=Bearer ${token}
+
+    Set Suite Variable    &{headers}
+    Set Suite Variable    ${token}
+    Set Suite Variable    ${surveyStatus}
+
+
 Submit survey if necessary
     IF    ${surveyStatus} == True
         Log To Console  Survey has been completed
