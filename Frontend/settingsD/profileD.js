eb6ede8eeed6f3018450de6b5e260f4ed40367fa
ttinnar <tinde@hotmail.fi>
2024-04-30 13:26:38 +0300

    doctor password change

--- Frontend/settingsD/profileD.js
+++ Frontend/settingsD/profileD.js
@@ -1,4 +1,5 @@
 import { fetchData } from "../assets/fetch.js";
+import { showSnackbar } from "../snackbar.js"
 
 function showProfile() {
     const user_name = localStorage.getItem('user_name');
@@ -10,6 +11,46 @@ function showProfile() {
     const emailSpan = document.getElementById("email");
     emailSpan.textContent = user_email || "No email available";
 }
+
+document.getElementById('changePasswordLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('passwordModal').style.display = 'block';
+});
+
+document.querySelector('.close').addEventListener('click', function() {
+    document.getElementById('passwordModal').style.display = 'none';
+});
+
+document.getElementById('passwordForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    const newPassword = document.getElementById('newPassword').value;
+
+    const url = 'http://127.0.0.1:3000/api/users/doctor/change-password';
+    const token = localStorage.getItem("token");
+
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ new_password: newPassword })
+    };
+
+    try {
+        const response = await fetch(url, options);
+        if (response.ok) {
+            showSnackbar('Green','Salasana vaihdettu onnistuneesti');
+            document.getElementById('passwordModal').style.display = 'none';
+        } else {
+            throw new Error('Salasanan vaihto epäonnistui');
+        }
+    } catch (error) {
+        showSnackbar('Red','Salasanan vaihto epäonnistui')
+        console.log(error.message);
+    }
+});
+
 
 showProfile()
 
