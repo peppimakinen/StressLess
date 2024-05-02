33a6446132ee7cc0fb55fde7548e9b9e598780fc
Alekkivi <Aleksi.Kivilehto@metropolia.fi>
2024-05-02 17:54:48 +0300

    added comments to logger

--- Backend/src/middlewares/logger.mjs
+++ Backend/src/middlewares/logger.mjs
@@ -1,5 +1,10 @@
 
-// Log client API interactions to console
+/**
+ * Log client API interactions to console
+ * @param {Request} req
+ * @param {Response} res
+ * @param {Function} next
+ */
 const logger = (req, res, next) => {
   console.log('Logger: ', req.method, req.path);
   next();
