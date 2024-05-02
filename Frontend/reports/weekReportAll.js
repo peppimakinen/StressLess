321cb79d9efe0300242a877c6fc108f62dbe3c12
TiiVii <111729213+TiiVii@users.noreply.github.com>
2024-05-02 22:27:44 +0300

    Fixes to the weekreport if there is not weekreports yet

--- Frontend/reports/weekReportAll.js
+++ Frontend/reports/weekReportAll.js
@@ -1,7 +1,6 @@
 import { fetchData } from "../assets/fetch.js";
 import { convertToDDMMYYYY } from "./convertDay.js"
 
-// Function to handle click event on "Näytä raportti" links
 window.addEventListener('load', async (evt) => {
     evt.preventDefault();
     try {
@@ -16,46 +15,34 @@ window.addEventListener('load', async (evt) => {
         };
 
         const reportData = await fetchData(url, options); 
-        if (reportData == null ) {
-            const weekItem = document.querySelector('.weeks');
-            weekItem.textContent = "Käyttäjätililläsi ei ole vielä yhtään viikkoraporttia.";
-
-        } else {
-            console.log(reportData);
+        if (reportData && reportData.length > 0) {
             const weekNumber = document.querySelector('.weeks');
-            // Iterate over the properties of reportData to populate the week numbers
-            for (const weekNumberKey in reportData) {
-                if (reportData.hasOwnProperty(weekNumberKey)) {
-                    const week = reportData[weekNumberKey];
-                    //week
-                    const weekItem = document.createElement('li');
-                    weekItem.classList.add('week');
-                    weekItem.textContent = `Viikko ${week.week_number}`;
-        
-                    //date
-                    const dateDiv = document.createElement('div');
-                    dateDiv.classList.add('date');
-                    const start_date = convertToDDMMYYYY(`${week.week_start_date}`);
-                    const end_date = convertToDDMMYYYY(`${week.week_end_date}`);
-                    dateDiv.textContent = `${start_date} - ${end_date}`;
-        
-                    //reports
-                    const reportsDiv = document.createElement('div');
-                    reportsDiv.classList.add('reports');
-                    const reportLink = document.createElement('a');
-                    reportLink.href = `weekReport.html?week=${week.week_number}`;
-                    reportLink.textContent = 'Näytä raportti';
-                    reportsDiv.appendChild(reportLink);
-                    weekItem.appendChild(dateDiv);
-                    weekItem.appendChild(reportsDiv);
-                    weekNumber.appendChild(weekItem);
-                };
-            };
-        };
+            reportData.forEach((week) => {
+                const weekItem = document.createElement('li');
+                weekItem.classList.add('week');
+                weekItem.textContent = `Viikko ${week.week_number}`;
+
+                const dateDiv = document.createElement('div');
+                dateDiv.classList.add('date');
+                const start_date = convertToDDMMYYYY(week.week_start_date);
+                const end_date = convertToDDMMYYYY(week.week_end_date);
+                dateDiv.textContent = `${start_date} - ${end_date}`;
+
+                const reportsDiv = document.createElement('div');
+                reportsDiv.classList.add('reports');
+                const reportLink = document.createElement('a');
+                reportLink.href = `weekReport.html?week=${week.week_number}`;
+                reportLink.textContent = 'Näytä raportti';
+                reportsDiv.appendChild(reportLink);
+                weekItem.appendChild(dateDiv);
+                weekItem.appendChild(reportsDiv);
+                weekNumber.appendChild(weekItem);
+            });
+        } else {
+            const weekItem = document.querySelector('.weeks');
+            weekItem.textContent = "Käyttäjätilillesi ei ole vielä luotu yhtään viikkoraporttia. Tämä generoituu automaattisesti su-ma välisenä yönä.";
+        }
     } catch (error) {
         console.error('Error fetching report:', error);
-    };
+    }
 });
-
-            
-            
