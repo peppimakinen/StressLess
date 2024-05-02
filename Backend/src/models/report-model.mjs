0d83b6e74c9c3e65113f370abbbcc732f98de02b
Alekkivi <Aleksi.Kivilehto@metropolia.fi>
2024-05-02 15:42:53 +0300

    added comments to delete-self-model

--- Backend/src/models/report-model.mjs
+++ Backend/src/models/report-model.mjs
@@ -1,5 +1,11 @@
 import promisePool from '../utils/database.mjs';
 
+/**
+ * Get weeks that containe reports
+ * @async
+ * @param {int} userId
+ * @return {List} Found reports with report ID. Returns a error if list is empty
+ */
 const getAvailableReportDates = async (userId) => {
   try {
     const sql = `
@@ -16,11 +22,18 @@ const getAvailableReportDates = async (userId) => {
     }
     return rows;
   } catch (error) {
-    console.error('getAllReportsWithUserId', error);
+    console.error('getAvailableReportDates', error);
     return {error: 500, message: 'db error'};
   }
 };
 
+/**
+ * Get specific report with report ID
+ * @async
+ * @param {Int} userId
+ * @param {Int} reportId
+ * @return {Object} Result. Returns a error if result is empty
+ */
 const getReport = async (userId, reportId) => {
   try {
     const sql = `
@@ -54,11 +67,17 @@ const getReport = async (userId, reportId) => {
     }
     return rows[0];
   } catch (error) {
-    console.error('getAllReportsWithUserId', error);
+    console.error('getReport', error);
     return {error: 500, message: 'db error'};
   }
 };
 
+/**
+ * Get the date for first entry for specific user ID
+ * @async
+ * @param {Int} userId
+ * @return {Object} Result. Returns a error if result is empty
+ */
 const getFirstEntryDateByUserId = async (userId) => {
   try {
     const sql = `
@@ -77,11 +96,19 @@ const getFirstEntryDateByUserId = async (userId) => {
     }
     return rows[0];
   } catch (error) {
-    console.error('getFirstEntryDate', error);
+    console.error('getFirstEntryDateByUserId', error);
     return {error: 500, message: 'db error'};
   }
 };
 
+/**
+ * Get data from DiaryEntries and Measurements between two dates
+ * @async
+ * @param {Int} userId
+ * @param {Date} startDate
+ * @param {endDate} endDate
+ * @return {List} Result. Returns a empty list if result is null
+ */
 const getReportData = async (userId, startDate, endDate) => {
   try {
     const sql = `
@@ -102,7 +129,7 @@ const getReportData = async (userId, startDate, endDate) => {
     const [rows] = await promisePool.query(sql, params);
     return rows;
   } catch (error) {
-    console.error('getAnalysisData', error);
+    console.error('getReportData', error);
     return {error: 500, message: 'db error'};
   }
 };
