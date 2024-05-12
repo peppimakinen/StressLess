9823837273d0f3ebe134c67cb8098aa974b245d5
ttinnar <tinde@hotmail.fi>
2024-05-12 22:50:18 +0300

    toimiva lokaali

--- Frontend/survey/alkuk.js
+++ Frontend/survey/alkuk.js
@@ -331,6 +331,15 @@ if (doctorForm2) {
 
     const authToken = localStorage.getItem("token");
 
+    const activitiesInput = form.querySelector('input[name="activities"]');
+    const activities = activitiesInput ? JSON.parse(activitiesInput.value) : [];
+    
+    if (activities.length === 0) {
+        console.log('No activities added. Survey will not be submitted.');
+        showCustomAlert('Tapahtui virhe. Tarkista, että olet vastannut kaikkiin pakollisiin kysymyksiin ja yritä uudelleen.');
+        return; // Exit function if activities list is empty
+        }
+
     const options = {
         method: 'POST',
         headers: {
